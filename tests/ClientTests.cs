f4b732b1d87f45c72a16ef57a3098eb19f803567
dennisvg111 <dennisvg111@gmail.com>
2023-11-01 18:24:33 +0100

    Add test for OneDrive Client

--- tests/ClientTests.cs
+++ tests/ClientTests.cs
@@ -1,13 +1,21 @@
-﻿using Xunit;
+﻿using System.Threading.Tasks;
+using Xunit;
 
 namespace DG.OneDrive.Tests
 {
     public class ClientTests
     {
         [Fact]
-        public void Something()
+        public async Task GetUser_WithAccessToken_ReturnsUser()
         {
+            var client = new Client(EnvironmentClientInfoProvider.Instance);
 
+            client.SetAccessToken(EnvironmentClientInfoProvider.AccessToken);
+
+            var user = await client.GetUser();
+
+            Assert.NotNull(user);
+            Assert.Equal("https://graph.microsoft.com/v1.0/$metadata#users/$entity", user.odataContext);
         }
     }
 }
