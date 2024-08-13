97d2d58ec133eb7da453e56b06752f2a0c4122fe
dennisvg111 <dennisvg111@gmail.com>
2024-08-13 21:04:13 +0200

    Setup test for memory footprint of reading OneDrive stream

--- tests/ClientTests.cs
+++ tests/ClientTests.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using DG.Common;
+using System;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
@@ -68,6 +71,70 @@ namespace DG.OneDrive.Tests
 
                     Assert.Equal(expectedContent, content);
                 }
+            }
+        }
+
+        private static readonly Process _currentProcess = Process.GetCurrentProcess();
+
+        [Fact()]//Skip = "Large memory footprint test, only run in local environments")]
+        public async Task CopyTo_MemoryFootprint()
+        {
+            var client = SetupClient();
+            var id = "667B8052A954FAAB!28514";
+
+            _currentProcess.Refresh();
+            var usedMemoryBefore = _currentProcess.PrivateMemorySize64;
+
+            using (var file = new DummyFile())
+            {
+                await client.CopyToStreamAsync(id, file.GetStream());
+            }
+
+            _currentProcess.Refresh();
+            var usedMemoryAfter = _currentProcess.PrivateMemorySize64;
+            var difference = ByteSize.FromBytes(usedMemoryAfter - usedMemoryBefore);
+
+            Assert.True(usedMemoryBefore < ByteSize.FromMB(2), $"Expected less than 500MB, actual {usedMemoryBefore}");
+        }
+
+        internal class DummyFile : IDisposable
+        {
+            private readonly string _name;
+            private bool disposedValue;
+            private FileStream fs;
+
+            public override string ToString()
+            {
+                return _name;
+            }
+
+            public DummyFile()
+            {
+                Directory.CreateDirectory(@"C:\tmp");
+                _name = @"C:\tmp\" + Guid.NewGuid().ToString().ToLowerInvariant() + ".dummy";
+                fs = new FileStream(_name, FileMode.CreateNew);
+            }
+
+            public Stream GetStream()
+            {
+                return fs;
+            }
+
+            protected virtual void Dispose(bool disposing)
+            {
+                if (!disposedValue)
+                {
+                    fs.Dispose();
+                    File.Delete(_name);
+                }
+                disposedValue = true;
+            }
+
+            public void Dispose()
+            {
+                // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+                Dispose(disposing: true);
+                GC.SuppressFinalize(this);
             }
         }
     }
